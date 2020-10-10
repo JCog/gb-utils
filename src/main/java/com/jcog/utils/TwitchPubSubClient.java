5d4b6f0f23329d6784d2405dc270a7529fc09457
JCog <jordancogan4@gmail.com>
2020-10-10 17:21:50 -0400

    rework TwitchPubSubClient slightly

--- src/main/java/com/jcog/utils/TwitchPubSubClient.java
+++ src/main/java/com/jcog/utils/TwitchPubSubClient.java
@@ -8,34 +8,33 @@ import com.github.twitch4j.pubsub.events.ChannelSubGiftEvent;
 import com.github.twitch4j.pubsub.events.RewardRedeemedEvent;
 
 public abstract class TwitchPubSubClient {
+    private final String streamerId;
+    private final TwitchPubSub client;
+    private final OAuth2Credential oAuth2Credential;
 
-    public TwitchPubSubClient(
-            String streamerId,
-            String authToken,
-            boolean listenForBits,
-            boolean listenForChannelPoints,
-            boolean listenForSubGifts
-    ) {
-        TwitchPubSub client = TwitchPubSubBuilder.builder().build();
-        OAuth2Credential oAuth2Credential = new OAuth2Credential("twitch", authToken);
-
-        if (listenForBits) {
-            client.listenForCheerEvents(oAuth2Credential, streamerId);
-            client.getEventManager().onEvent(ChannelBitsEvent.class, this::onBitsEvent);
-        }
-
-        if (listenForChannelPoints) {
-            client.listenForChannelPointsRedemptionEvents(oAuth2Credential, streamerId);
-            client.getEventManager().onEvent(RewardRedeemedEvent.class, this::onChannelPointsEvent);
-        }
-
-        if (listenForSubGifts) {
-            client.listenForChannelSubGiftsEvents(oAuth2Credential, streamerId);
-            client.getEventManager().onEvent(ChannelSubGiftEvent.class, this::onSubGiftsEvent);
-        }
+    public TwitchPubSubClient(String streamerId, String authToken) {
+        this.streamerId = streamerId;
+        client = TwitchPubSubBuilder.builder().build();
+        oAuth2Credential = new OAuth2Credential("twitch", authToken);
     }
 
+    public TwitchPubSubClient listenForBits() {
+        client.listenForCheerEvents(oAuth2Credential, streamerId);
+        client.getEventManager().onEvent(ChannelBitsEvent.class, this::onBitsEvent);
+        return this;
+    }
 
+    public TwitchPubSubClient listenForChannelPoints() {
+        client.listenForChannelPointsRedemptionEvents(oAuth2Credential, streamerId);
+        client.getEventManager().onEvent(RewardRedeemedEvent.class, this::onChannelPointsEvent);
+        return this;
+    }
+
+    public TwitchPubSubClient listenForSubGifts() {
+        client.listenForChannelSubGiftsEvents(oAuth2Credential, streamerId);
+        client.getEventManager().onEvent(ChannelSubGiftEvent.class, this::onSubGiftsEvent);
+        return this;
+    }
 
     public abstract void onBitsEvent(ChannelBitsEvent event);
 
